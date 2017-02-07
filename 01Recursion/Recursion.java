90084cfbcf5982a4b17f95d832093587a50487e6
Laura Goon <laura.gooon@gmail.com>
2017-02-07 13:06:18 -0500

    initial

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,10 +1,18 @@
-public class Recursion{
-
-  public static String name(){
-    return "Goon,Laura";
+ublic class Recursion{ 
+ public static String name(){
+  return "Goon,Laura";
+ }
+ public static double helper(double n, double guess){
+  if (n < 0){
+   throw new
   }
-
-  public static double sqrt(double n){
-    
+  double better_guess = ((n / guess) + guess) / 2;
+  if (Math.abs(better_guess - guess) < 0.0000000000001){
+   return better_guess;
   }
+  return helper(n, better_guess);
+ }
+ public static double sqrt(double n){ 
+  return helper(n,n/2);
+ }
 }
