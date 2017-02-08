25cf449090d9bd70a5ce8d32e99f9fd2af5986f7
Laura <laura.gooon@gmail.com>
2017-02-07 21:28:28 -0500

    finish sqrt and helper fxns

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,18 +1,23 @@
-ublic class Recursion{ 
- public static String name(){
-  return "Goon,Laura";
- }
- public static double helper(double n, double guess){
-  if (n < 0){
-   throw new
+public class Recursion{
+  public static String name(){
+    return "Goon,Laura";
   }
-  double better_guess = ((n / guess) + guess) / 2;
-  if (Math.abs(better_guess - guess) < 0.0000000000001){
-   return better_guess;
+
+  private static double helper(double n, double guess){
+    if (n < 0){
+      throw new IllegalArgumentException();
+    }
+    if (n == 0){
+      return 0;
+    }
+    double better_guess = ((n / guess) + guess) / 2;
+    if (Math.abs(better_guess - guess) < 0.0000000000001){
+      return better_guess;
+    }
+    return helper(n, better_guess);
   }
-  return helper(n, better_guess);
- }
- public static double sqrt(double n){ 
-  return helper(n,n/2);
- }
+
+  public static double sqrt(double n){
+    return helper(n, n/2);
+  }
 }
