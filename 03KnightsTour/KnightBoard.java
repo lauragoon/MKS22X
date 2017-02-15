ec5392410b7a63fdd010c00833b7e88ba04e08c4
Laura Goon <laura.gooon@gmail.com>
2017-02-15 14:46:05 -0500

    more things made the tostring and constructor

--- 03KnightsTour/KnightBoard.java
+++ 03KnightsTour/KnightBoard.java
@@ -1,19 +1,75 @@
 public class KnightBoard{
+    private int[][] board;
 
+    public KnightBoard(int startingRows, int startingCols){
+	board = new int[startingRows][startingCols];
+	for (int r = 0; r < board.length; r++){               // clear
+	    for (int c = 0; c < board[r].length; c++){
+		board[r][c] = 0;
+	    }
+	}
+    }
 
-  public KnightBoard(int startingRows, int startingCols){
-    //bod
-  }
+    /* do i need this
+    private boolean addStep(int row, int col){
+	if (board[row][col] != 0){
+	    return false;
+	} else{
+	    board[row][col] = ll
+	}
+    }*/
 
-  private boolean solveH(int row, int col, int level){
-    //bod
-  }
+    private boolean solveH(int row, int col, int level){
+	int numRows = board.length;
+	int numCols = board[0].length;
+	// int[] ah = {12,-8,21,19,-19,-21,8,-12};
+	
+	if (level > numRows * numCols){
+	    return true; // should be done
+	}
+	while (level <= numRows * numCols){
+	    
+	}
 
-  public void solve(){
-    //bod
-  }
+	/*
+        for (int r = row; r < numRows; r++){
+	    for (int c = col; c < numCols; c++){
+		//starts at designated
+		if (
+	    }
+	    col = 0;
+	    }*/
+	
+	/*
+	00 01 02 03 04
+        10 11 12 13 14
+	20 21 22 23 24
+        30 31 32 33 34
+	*/
+    }
 
-  public String toString(){
-    //bod
-  }
+    public void solve(){
+	solveH(0,0,1);
+    }
+
+    public String toString(){
+	String ans = "";
+	for (int r = 0; r < board.length; r++){
+	    for (int c = 0; c < board[r].length; c++){
+		if (board[r][c] < 10){
+		    ans += " " + Integer.toString(board[r][c]) + " ";
+		} else{
+		    ans += Integer.toString(board[r][c]) + " ";
+		}
+	    }
+	    ans += "\n";
+	}
+	return ans;
+    }
+
+    public static void main(String[] args){
+	Knightboard test = new Knightboard(5,5);
+	test.solve();
+	System.out.println(test);
+    }
 }
