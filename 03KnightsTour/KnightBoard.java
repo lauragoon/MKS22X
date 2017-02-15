c224b94de61fedf81a9415897489d03db62376ba
Laura <laura.gooon@gmail.com>
2017-02-14 22:43:31 -0500

    initial commit

---
+++ 03KnightsTour/KnightBoard.java
@@ -0,0 +1,19 @@
+public class KnightBoard{
+
+
+  public KnightBoard(int startingRows, int startingCols){
+    //bod
+  }
+
+  private boolean solveH(int row, int col, int level){
+    //bod
+  }
+
+  public void solve(){
+    //bod
+  }
+
+  public String toString(){
+    //bod
+  }
+}
