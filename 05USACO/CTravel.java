84bb935b96c9972b512a5845ae407c16f959da14
Laura <laura.gooon@gmail.com>
2017-03-12 00:15:50 -0500

    cow travel cleanup

--- 05USACO/CTravel.java
+++ 05USACO/CTravel.java
@@ -49,20 +49,12 @@ public class CTravel {
       pasture[R1][C1] = 1;
 
       int counter = 1;
-      // System.out.println(debugArr(pasture));
-      // System.out.println(debugArr(pasture2));
       while (counter <= T) {
-        // if (counter == 1) {
-        //   move(pasture,pasture2,true);
-        // }else {
           if (counter % 2 != 0) {
             move(pasture,pasture2);
-            // System.out.println(debugArr(pasture2));
           } else {
             move(pasture2,pasture);
-            // System.out.println(debugArr(pasture));
           }
-        // }
         counter++;
       }
 
@@ -79,129 +71,37 @@ public class CTravel {
   }
 
   public void move(int[][] from, int[][] to){
-    // if (isFirst) {
-    //   for (int row = 0; row < N; row++) {
-    //     for (int col = 0; col < M; col++) {
-    //       to[row][col] = template[row][col];
-    //     }
-    //   }
-    // } else {
-      for (int row = 0; row < N; row++) {
-        for (int col = 0; col < M; col++) {
-          to[row][col] = template[row][col];
-        }
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < M; col++) {
+        to[row][col] = template[row][col];
       }
-    // }
+    }
 
-    // try {
-    //   TimeUnit.SECONDS.sleep(3);
-    // } catch (InterruptedException e) {
-    //   System.out.println("Why");
-    // }
     for (int r = 0; r < N; r++) {
-      // System.out.println("STARTING NEW ROW, ROW #:" + Integer.toString(r+1));
       for (int c = 0; c < M; c++) {
-        // System.out.println("STARTING NEW COL, COL #:" + Integer.toString(c+1));
-        // System.out.println("(" + Integer.toString(r+1) + ", " + Integer.toString(c+1) + ")");
-        // System.out.println();
-        // System.out.println("FROM");
-        // System.out.println(debugArr(from));
-        // System.out.println("TO");
-        // System.out.println(debugArr(to));
-        // System.out.println("BEG");
-        // System.out.println();
-        // try {
-        //   TimeUnit.SECONDS.sleep(3);
-        // } catch (InterruptedException e) {
-        //   System.out.println("Why");
-        // }
-
         if (from[r][c] != 0 && from[r][c] != -1) { // top left
+
           if (r != 0 && to[r-1][c] != -1) {
             to[r-1][c] += from[r][c];
-
-            // System.out.println("case 1"); // TOP ROW
-            // System.out.println("FROM");
-            // System.out.println(debugArr(from));
-            // System.out.println("TO");
-            // System.out.println(debugArr(to));
-            // try {
-            //   TimeUnit.SECONDS.sleep(3);
-            // } catch (InterruptedException e) {
-            //   System.out.println("Why");
-            // }
           }
 
           if (r < N-1 && to[r+1][c] != -1) {
             to[r+1][c] += from[r][c];
-
-            // System.out.println("case 2"); // LAST ROW
-            // System.out.println("FROM");
-            // System.out.println(debugArr(from));
-            // System.out.println("TO");
-            // System.out.println(debugArr(to));
-            // try {
-            //   TimeUnit.SECONDS.sleep(3);
-            // } catch (InterruptedException e) {
-            //   System.out.println("Why");
-            // }
           }
 
           if (c != 0 && to[r][c-1] != -1) {
             to[r][c-1] += from[r][c];
-
-            // System.out.println("case 3"); // FIRST COLs
-            // System.out.println("FROM");
-            // System.out.println(debugArr(from));
-            // System.out.println("TO");
-            // System.out.println(debugArr(to));
-            // try {
-            //   TimeUnit.SECONDS.sleep(3);
-            // } catch (InterruptedException e) {
-            //   System.out.println("Why");
-            // }
           }
 
           if (c < M-1 && to[r][c+1] != -1) {
             to[r][c+1] += from[r][c];
-
-            // System.out.println("case 4"); // LAST COL
-            // System.out.println("FROM");
-            // System.out.println(debugArr(from));
-            // System.out.println("TO");
-            // System.out.println(debugArr(to));
-            // try {
-            //   TimeUnit.SECONDS.sleep(3);
-            // } catch (InterruptedException e) {
-            //   System.out.println("Why");
-            // }
           }
+
           to[r][c] = 0;
-          // System.out.println("reset");
-          // System.out.println("FROM");
-          // System.out.println(debugArr(from));
-          // System.out.println("TO");
-          // System.out.println(debugArr(to));
-          // try {
-          //   TimeUnit.SECONDS.sleep(3);
-          // } catch (InterruptedException e) {
-          //   System.out.println("Why");
-          // }
         }
       }
     }
   }
-// }
 
-  public String debugArr(int[][] arr) {
-      String ret = "";
-      for (int i = 0; i < N; i++){
-        for (int j = 0; j < M; j++){
-          ret += arr[i][j] + " ";
-        }
-        ret += "\n";
-      }
-      return ret;
-  }
 
 }
--- 05USACO/USACO.java
+++ 05USACO/USACO.java
@@ -13,24 +13,4 @@ public class USACO {
     return cow.S;
   }
 
-  public static void main(String[] args) {
-    USACO x = new USACO();
-    // System.out.println(x.silver("tests2/ctravel.3.in"));
-    // System.out.println("6435");
-    // System.out.println(x.silver("tests2/ctravel.4.in"));
-    // System.out.println("339246");
-    // System.out.println(x.silver("tests2/ctravel.5.in"));
-    // System.out.println("0");
-    // System.out.println(x.silver("tests2/ctravel.6.in"));
-    // System.out.println("14396412");
-    // System.out.println(x.silver("tests2/ctravel.7.in"));
-    // System.out.println("1533810");
-    // System.out.println(x.silver("tests2/ctravel.8.in"));
-    // System.out.println("456055");
-    // System.out.println(x.silver("tests2/ctravel.9.in"));
-    // System.out.println("28");
-    // System.out.println(x.silver("tests2/ctravel.10.in"));
-    // System.out.println("1321670");
-  }
-
 }
