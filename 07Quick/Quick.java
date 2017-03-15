50009ec9301ea1ea5bbeaa9072190c3bd1e26a39
Laura Goon <laura.gooon@gmail.com>
2017-03-15 14:45:47 -0400

    lol

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -1,22 +1,58 @@
 import java.util.Random;
 
-public class placeholder {
-  public placeholder() {
+public class Quick {
+    int[] ary = {2,10,15,23,0,5};
+    int splittingOn;
+    int[] test;
+
+  public Quick() {
     //bod
   }
 
-  public int partition(int[] arr, int start, int end) { // end and start in indices
+  public static void partition(int[] arr, int start, int end) { // end and start in indices
     Random rand = new Random();
     int ranInd = rand.nextInt(end-start) + 1;
     int splitOn = arr[ranInd];
+    int splittingOn = splitOn;
 
+    while (start <= end){
+	while (arr[start] < splitOn) start++;
+	while (arr[end] > splitOn) end--;
+	if (start <= end) swap(arr[start++], arr[end--]);
+    }
+    /*
+    int[] ret = new int[arr.length];
+    
+    for (int i = start; i <= end; i++){
+	if (arr[i] == splitOn){
+	    // nothing
+	} else if (i == end) {
+	    if (arr[i] < splitOn){
+		ret[start] = arr[i];
+		start++;
+		ret[start] = splitOn;
+	    } else if (arr[i] > splitOn){
+		ret[end] = arr[i];
+		end--;
+		ret[end] = splitOn;
+	    }
+	} else if (arr[i] < splitOn){
+	    ret[start] = arr[i];
+	    start++;
+	} else if (arr[i] > splitOn) {
+	    ret[end] = arr[i];
+	    end--;
+	}
+    }
+    */
+    /*
     int[] ret = new int[arr.length];
     int begging = 0;
     int ending = end;
-    int lastHighest;
+    int lastHighest = end;
 
     for (int i = 0; i <= end; i++) {
-      if (i == end) {
+	if (i == end) { // if it at end or not
         int temp = ret[lastHighest];
         ret[lastHighest] = splitOn;
         ret[end] = temp;
@@ -31,13 +67,36 @@ public class placeholder {
         ending--;
       }
     }
-
+    */
+    /*
     if (end < arr.length-1) {
       for (int i = end + 1; i < arr.length; i++) {
         ret[i] = arr[i];
       }
     }
-
-    arr = ret;
+    */
+    /*
+    // arr = ret;
+    
+    for (int i = 0; i < arr.length; i++){
+	arr[i] = ret[i];
+    }
+    */
   }
+    public static String toString(int[] arr){
+	String ret = "[";
+	for (int i = 0; i < arr.length; i++){
+	    ret += arr[i] + ", ";
+	}
+	ret += "]";
+	return ret;
+    }
+
+    public static void main(String[] args){
+        Quick hello = new Quick();
+	partition(hello.ary,0,hello.ary.length-1);
+	System.out.println(hello.splittingOn);
+	System.out.println(toString(hello.ary));
+	//System.out.println(toString(hello.test));
+    }
 }
