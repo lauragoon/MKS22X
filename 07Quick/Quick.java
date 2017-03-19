ca78bdf8e951fa88e4885f3d750dd60fbab3e541
Laura <laura.gooon@gmail.com>
2017-03-18 22:32:03 -0400

    quickselect() and quicksort() should work.....

--- 07Quick/Quick.java
+++ 07Quick/Quick.java
@@ -4,6 +4,12 @@ public class Quick {
   public Quick() {
   }
 
+  public static void switching(int[] arr, int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+
   public static int part (int[] data, int start, int end){
     //-Choose a random element to be a pivot, and partition the array around it.
     //-Only partition the elements from start to end inclusive.
@@ -11,60 +17,29 @@ public class Quick {
     //    (Should be from start to end inclusive)
     Random rand = new Random();
     int ranInd = rand.nextInt(end-start+1) + start;
-    int splitOn = data[ranInd];
 
-    int[] work = new int[data.length];
-    int start2 = start;
-    int end2 = end;
-    boolean passPivot = false;
-    // int newIndex = 0;
+    switching(data,ranInd,end);
+    int splitOn = data[end];
+    int newIndex = 0;
 
-    // System.out.println("PIVOT :" + Integer.toString(splitOn));
-
-    for (int i = start; i < end; i++) {
-      if (i == ranInd) {
-        passPivot = true;
-      }
-      if ((data[i] < splitOn) || (data[i] == splitOn && passPivot)) {
-        work[start2] = data[i];
-        start2++;
-        // System.out.println("CASE 1, BEG");
-        // System.out.println(toString(data));
-        // System.out.println(i);
-        // System.out.println(toString(work));
-      }
-      if (data[i] > splitOn) {
-        work[end2] = data[i];
-        end2--;
-        // System.out.println("CASE 2, END");
-        // System.out.println(toString(data));
-        // System.out.println(i);
-        // System.out.println(toString(work));
+    for (int i = 0; i < end; i++) {
+      if (data[i] <= splitOn) {
+        switching(data,i,newIndex);
+        newIndex++;
       }
     }
-    work[start2] = splitOn;
-    // newIndex = start2;
-    // System.out.println("ENDING IT ALL");
-    // System.out.println(start2);
-    // System.out.println(end2);
-    // System.out.println(toString(work));
-
-    for (int i = start; i <= end; i++) {
-      data[i] = work[i];
-    }
-
-    // return toString(data) + " on: " + Integer.toString(splitOn) + "new Index: " + Integer.toString(newIndex);
-    return start2;
+    switching(data,newIndex,end);
+    return newIndex;
   }
 
   public static int quickselect(int[] data, int k){
     //return the value that is the kth smallest value of the array.
     //use your partition method to help you accomplish this.
-    return quickselectH(data,k,0,data.length);
+    return quickselectH(data,k,0,data.length-1);
   }
 
   public static int quickselectH(int[] data, int k, int start, int end) {
-    int ind = part(data,0,data.length);
+    int ind = part(data,0,data.length-1);
     if (ind == k) {
       return data[k];
     } if (k > ind) {
@@ -74,38 +49,25 @@ public class Quick {
     }
   }
 
-  public static String toString(int[] arr){
-    String ret = "[";
-    for (int i = 0; i < arr.length; i++){
-      ret += arr[i] + ", ";
-    }
-    ret += "]";
-    return ret;
+  public static void quicksort(int[] data) {
+    quicksortH(data,0,data.length-1);
   }
 
-  public static void main(String[] args) {
-    // int[] test = {6,7,1,2,3,4,0,9};
-    // int[] test = {999,999,999,4,1,0,3,2,999,999,999};
-    // System.out.println(toString(test));
-    // System.out.println(part(test,3,7));
-    int[] ary = {2,10,15,23,0,5};
-    System.out.println(quickselect(ary,0));
-    System.out.println(0);
-    System.out.println();
-    System.out.println(quickselect(ary,1));
-    System.out.println(2);
-    System.out.println();
-    System.out.println(quickselect(ary,2));
-    System.out.println(5);
-    System.out.println();
-    System.out.println(quickselect(ary,3));
-    System.out.println(10);
-    System.out.println();
-    System.out.println(quickselect(ary,4));
-    System.out.println(15);
-    System.out.println();
-    System.out.println(quickselect(ary,5));
-    System.out.println(23);
+  public static void quicksortH(int[] data, int left, int right) {
+    if (left < right) {
+      int p = part(data,left,right);
+      quicksortH(data,p+1,right);
+      quicksortH(data,left,p-1);
+    }
   }
+
+  // public static String toString(int[] arr){
+  //   String ret = "[";
+  //   for (int i = 0; i < arr.length; i++){
+  //     ret += arr[i] + ", ";
+  //   }
+  //   ret += "]";
+  //   return ret;
+  // }
 
 }
