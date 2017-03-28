1afdff8fe990a63ef0bd7c64141359978ba31b3c
Laura <laura.gooon@gmail.com>
2017-03-28 00:36:09 -0400

    initial x2

--- 09MyLinkedList/MyLinkedList.java
+++ 09MyLinkedList/MyLinkedList.java
@@ -1,3 +1,15 @@
 public class MyLinkedList {
-  
+  private LNode start;
+  public int size;
+
+  public void add(int n) {
+
+  }
+
+  public String toString() {
+    
+  }
+
+  private class LNode{
+  }
 }
