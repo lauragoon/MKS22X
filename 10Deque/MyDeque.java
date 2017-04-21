5af5e7d09cbf4708f1e07d4d79c083125b2f15a6
Laura Goon <laura.gooon@gmail.com>
2017-04-21 14:43:52 -0400

    start

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,3 +1,21 @@
 public class MyDeque {
+    public void addFirst(String str) {
+    }
+
+    public void addLast(String str) {
+    }
+
+
+    public String removeFirst() {
+    }
     
+    public String removeLast() {
+    }
+
+
+    public String getFirst() {
+    }
+
+    public String getLast() {
+    }
 }
