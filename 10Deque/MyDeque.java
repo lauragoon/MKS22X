135a849e360651fa1d344a4604fde21cc34853b7
Laura <laura.gooon@gmail.com>
2017-04-24 00:51:03 -0400

    hello it works

--- 10Deque/MyDeque.java
+++ 10Deque/MyDeque.java
@@ -1,21 +1,127 @@
+import java.util.NoSuchElementException;
+
 public class MyDeque {
-    public void addFirst(String str) {
-    }
+  private String[] data;
+  private int head, tail, size;
 
-    public void addLast(String str) {
+  public MyDeque() {
+    data = new String[10];
+    head = 0;
+    tail = 0;
+    size = 0;
+  }
+
+  public void addFirst(String str) {
+    if (str == null) {
+      throw new NullPointerException();
     }
+    if (size == 0) {
+      data[head] = str;
+    }
+    else {
+      if (size == data.length) {
+        doubleSize();
+      }
+      head = Math.floorMod(head - 1, data.length);
+      data[head] = str;
+    }
+    size++;
+  }
+
+  public void addLast(String str) {
+    if (str == null) {
+      throw new NullPointerException();
+    }
+    if (size == 0) {
+      data[tail] = str;
+    }
+    else {
+      if (size == data.length) {
+        doubleSize();
+      }
+      tail = Math.floorMod(tail + 1, data.length);
+      data[tail] = str;
+    }
+    size++;
+  }
 
 
-    public String removeFirst() {
+  public String removeFirst() {
+    if (size == 0) {
+      throw new NoSuchElementException();
     }
-    
-    public String removeLast() {
+    String ret = data[head];
+    size--;
+    if (size == 1) {
+      head = 0;
+      tail = 0;
+      return ret;
     }
+    head = Math.floorMod(head + 1, data.length);
+    return ret;
+  }
+
+  public String removeLast() {
+    if (size == 0) {
+      throw new NoSuchElementException();
+    }
+    String ret = data[tail];
+    size--;
+    if (size == 1) {
+      head = 0;
+      tail = 0;
+      return ret;
+    }
+    tail = Math.floorMod(tail - 1, data.length);
+    return ret;
+  }
 
 
-    public String getFirst() {
+  public String getFirst() {
+    if (size == 0) {
+      throw new NoSuchElementException();
     }
+    return data[head];
+  }
 
-    public String getLast() {
+  public String getLast() {
+    if (size == 0) {
+      throw new NoSuchElementException();
     }
+    return data[tail];
+  }
+
+  public String toString() {
+    String ret = "[";
+    if (size == 0) {
+      ret += "]";
+    }
+    else {
+      int i = head;
+      while (i != tail) {
+        ret += data[i] + ", ";
+        i = Math.floorMod(i+1, data.length);
+      }
+      ret += data[i] + "]";
+    }
+    return ret;
+  }
+
+
+  private void doubleSize() {
+    String[] hello = new String[data.length * 2];
+    int ind = head;
+    int ind2 = 0;
+    while (ind != tail) {
+      hello[ind2] = data[ind];
+      ind = Math.floorMod(ind+1,data.length);
+      ind2++;
+    }
+    hello[ind2] = data[tail];
+    head = 0;
+    tail = ind2;
+    data = hello;
+  }
+
+
 }
