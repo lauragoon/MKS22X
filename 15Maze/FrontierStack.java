5df8c1f1490a8d7aa375cab7cd408706a390492b
Laura Goon <goon.laura@gmail.com>
2017-05-19 10:37:22 -0400

    stacked it done

--- 15Maze/FrontierStack.java
+++ 15Maze/FrontierStack.java
@@ -2,13 +2,15 @@ public class FrontierStack implements Frontier {
     private Stack<Location> locations;
 
     public FrontierStack() {
-	locations = new Stack();
+	locations = new Stack()<Location>;
     }
     
     public void add(Location loc) {
+	locations.push(loc);
     }
 
     public Location next() {
+	locations.pop();
     }
 }
 
